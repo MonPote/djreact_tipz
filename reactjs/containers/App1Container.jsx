447b43d75393160d626d50d9b7d20bb978ed5cbf
EAR Patrick <patrick-hk@outlook.com>
2017-01-06 17:23:30 +0100

    [DEV] Add hot reloading

--- reactjs/containers/App1Container.jsx
+++ reactjs/containers/App1Container.jsx
@@ -8,7 +8,7 @@ export default class App1Container extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Headline>Hello World!</Headline>
+            <Headline>Hugo !</Headline>
           </div>
         </div>
       </div>
